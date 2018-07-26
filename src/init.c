5b9ea4f3077b618290c977e8400623246b8d3bfd
FocusPaka <2160090406@email.szu.edu.cn>
2018-07-26 21:37:31 +0800

    Adjust code format, and revise the code for register native routines.

--- src/init.c
+++ src/init.c
@@ -2,19 +2,20 @@
 #include <R_ext/Rdynload.h>
 
 /* FIXME: 
-   Check these declarations against the C/Fortran source code.
+  Check these declarations against the C/Fortran source code.
 */
+  
+  /* .C calls */
+extern void pvalueclassify(void *, void *, void *, void *, void *, void *, 
+                           void *, void *, void *, void *, void *, void *);
 
-/* .C calls */
-extern void pvalueclassify(void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *, void *);
-
-static const R_CMethodDef CEntries[] = {
-    {"pvalueclassify", (DL_FUNC) &pvalueclassify, 12},
-    {NULL, NULL, 0}
+static const R_CMethodDef cMethods[] = {
+  {"pvalueclassify", (DL_FUNC) & pvalueclassify, 12},
+  NULL
 };
 
-void R_init_SIMD(DllInfo *dll)
+void R_init_SIMD(DllInfo *info)
 {
-    R_registerRoutines(dll, CEntries, NULL, NULL, NULL);
-    R_useDynamicSymbols(dll, FALSE);
+  R_registerRoutines(info, cMethods, NULL, NULL, NULL);
+  R_useDynamicSymbols(info, FALSE);
 }
