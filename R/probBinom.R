5783929b6eaba46b9f25aa62780c48b30a1cb156
FocusPaka <2160090406@email.szu.edu.cn>
2018-07-26 16:59:36 +0800

    Adjust the code format to follow the coding style.

--- R/probBinom.R
+++ R/probBinom.R
@@ -12,29 +12,29 @@
 #' CpG sites.
 #' @examples 
 #' set.seed(1234)
-#' t<-0.1
-#' size1<-sample(1:1000, 1, replace=TRUE)
-#' size2<-sample(1:1000, 1, replace=TRUE)
-#' c1<-1
-#' c2<-2
-#' result<-probBinom(t,size1,size2,c1,c2)
+#' t <- 0.1
+#' size1 <- sample(1:1000, 1, replace=TRUE)
+#' size2 <- sample(1:1000, 1, replace=TRUE)
+#' c1 <- 1
+#' c2 <- 2
+#' result <- probBinom(t, size1, size2, c1, c2)
 #' @export
 
 
-probBinom<-function(t,size1,size2,c1,c2){
-    prob1<-1/(1+c1)
-    prob2<-1/(1+c2)
-    i<-0:size1
-    j<-size1:0
-    pij<- dbinom(i,prob=prob1,size=size1)
-    r<-0:size2
-    k<-size2:0
-    prk<- dbinom(r,prob=prob2,size=size2)
-    ik<-outer(i,k)
-    jr<-outer(j,r)
-    ijrk<-c1*ik-c2*jr
-    prob<-outer(pij,prk)
-    ir<-which(abs(ijrk)>=t, arr.ind=TRUE)
-    p<-sum(prob[ir])
+probBinom <- function(t, size1, size2, c1, c2){
+    prob1 <- 1/(1 + c1)
+    prob2 <- 1/(1 + c2)
+    i <- 0:size1
+    j <- size1:0
+    pij <- dbinom(i, prob=prob1, size=size1)
+    r <- 0:size2
+    k <- size2:0
+    prk <- dbinom(r, prob=prob2, size=size2)
+    ik <- outer(i, k)
+    jr <- outer(j, r)
+    ijrk <- c1*ik-c2*jr
+    prob <- outer(pij, prk)
+    ir <- which(abs(ijrk)>=t, arr.ind=TRUE)
+    p <- sum(prob[ir])
     return(p)
 }
